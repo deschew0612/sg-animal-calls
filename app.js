a717a11c1d9467942bcbb125df6064e00c21f6cb
deschew0612 <desireechew.0612@gmail.com>
2024-05-22 09:45:25 +0000

    added comments to app.js, dynamic table input for sources.ejs

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+/*import Express module (webapp framework for node.js) and create an app instance*/
 const express = require('express');
 const app = express();
+
+/*import path module for file and directory paths*/
 const path = require('path');
+
+/*import the array of animals in constants module*/
 const constants = require('./lib/constants.js');
 
+/*set up the view engine for the Express app to ejs (embedded js)*/
 app.set('view engine', 'ejs');
+/*Serve static files from the 'public' dir*/
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*unpack the array of animals in the constants module (akin to a container)*/
 const animals = constants.animals
 
-/* show index.ejs with animal grid */
+/* Define home route -- render the index.ejs template and pass animals array to template */
 app.get('/', (req, res) => {
     res.render('index', { animals });
 });
 
+/*Define animals JSON route for filter function -- when accessed, it responds with a json representation of the 'animals' array*/
 app.get('/animals', (_req, res) => {
     res.json(animals);
 })
 
-/* show animal.ejs based on animal id clicked*/
+/* Define indiv animal route: extract the 'id' parameter from the request, retrieve the corresponding animal from 'animals' array, render the animal.ejs template with the specific animal's data*/
 app.get('/animals/:id', (req, res) => {
     const { id } = req.params
 
@@ -26,11 +35,12 @@ app.get('/animals/:id', (req, res) => {
     res.render('animal', { animal });
 });
 
-/* show sources.ejs when user clicks 'Sources'*/
+/* define route -- render the sources.ejs template*/ 
 app.get('/sources', (req, res) => {
-    res.render('sources');
+    res.render('sources',{ animals });
 });
 
+/*start the server*/
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
